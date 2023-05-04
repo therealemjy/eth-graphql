d947dda212fbfd503a4aa7cc083dc0a556bdf9e5
therealemjy <maxime.julian.creations@gmail.com>
2023-05-04 17:22:52 +0100

    fork mainnet during tests

--- packages/e2e-tests/hardhat.config.ts
+++ packages/e2e-tests/hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env['MAINNET_FORKING_RPC_URL'] || '',
+      }
+    }
+  }
 };
 
 export default config;
