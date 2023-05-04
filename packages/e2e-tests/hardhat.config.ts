4705e5bb18ef07ef51b2a107fee61a1dbca3a066
therealemjy <maxime.julian.creations@gmail.com>
2023-05-04 17:39:05 +0100

    add Prettier to e2e-tests

--- packages/e2e-tests/hardhat.config.ts
+++ packages/e2e-tests/hardhat.config.ts
@@ -1,16 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-
+import '@nomicfoundation/hardhat-toolbox';
+import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
   networks: {
     hardhat: {
       forking: {
         url: process.env['MAINNET_FORKING_RPC_URL'] || '',
-      }
-    }
-  }
+      },
+    },
+  },
 };
 
 export default config;
